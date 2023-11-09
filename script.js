23468312debb2c1d7ff23b9914d8467518273956
Prince Kumar Pandit <133173317+prince-k1@users.noreply.github.com>
2023-11-09 19:43:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 // your code here
+let heading = document.querySelector('h3');
+let Name = document.getElementById('name');
+let year = document.getElementById('year');
+
+let btn = document.getElementById('button');
+
+btn.addEventListener('click', () => {
+    let nameVal = Name.value;
+    let yearVal = year.value;
+    if(nameVal != "" && yearVal != ""){
+        heading.innerText = `https://localhost:8080/?name=${nameVal}&year=${yearVal}`;
+    }
+    else if(nameVal != "" && yearVal =="" ){
+        heading.innerText = `https://localhost:8080/?name=${nameVal}`;
+    }
+    else if(nameVal == "" && yearVal !="" ){
+        heading.innerText = `https://localhost:8080/?year=${yearVal}`;
+    }
+    Name.value = "";
+    year.value = "" ;
+})
